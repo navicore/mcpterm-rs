1e9b4a654be3d54abc3bcd77da1fcc6ab5a5e30d
Ed Sweeney <ed@onextent.com>
2025-05-09 20:40:56 -0700

    fresh start

--- archives/p1/run_with_debug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build the application first
-cargo build
-
-# Run mcpterm with RUST_BACKTRACE and RUST_LOG set
-echo "Starting mcpterm with verbose logging..."
-echo "Log files will be in your temp directory."
-RUST_BACKTRACE=1 RUST_LOG=debug ./target/debug/mcpterm "$@"
